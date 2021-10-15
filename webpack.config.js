a2ae8ce664869ab40d22a68d243f58e266362453
mabedalazim1 <mabedalazim1@gmail.com>
2021-10-15 15:01:53 +0200

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
+
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,6 +14,13 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
+        ],
+        Plugins: [
+            new HtmlWebpackPlugin({
+                template="./src/client/views/index.html",
+               
+            })
         ]
     }
+   
 }
